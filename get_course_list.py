2673b932eae3d9ad6a352da0853982d0e23c1d4e
noko <nokoaashish@gmail.com>
2025-03-05 07:11:03 +0545

    updated some code

--- get_course_list.py
+++ get_course_list.py
@@ -24,26 +24,27 @@ def get_course_list():
     
     homepage_lists = element = wait.until(EC.element_to_be_clickable((By.XPATH, "(//a[@class='flex mb-3']/span[@class])[1]")))
     homepage_lists.click()
+    time.sleep(2)
     
-    
-    total_chapters  = wait.until(EC.presence_of_all_elements_located((By.XPATH,'//h3/a')))
+    total_chapters  = driver.find_elements(By.XPATH,'//h3/a')
     for i in range(len(total_chapters)):
-        chapters  = wait.until(EC.presence_of_all_elements_located((By.XPATH,'//h3/a')))
+        chapters  = driver.find_elements(By.XPATH,'//h3/a')
         time.sleep(2)
         chapters[i].click()
         time.sleep(5)
         
         
         try:
-            redirect = wait.until(EC.element_to_be_clickable((By.XPATH,'//nav[@aria-label="Breadcrumb"]//a[contains(text(),"Introduction")]')))
+            redirect = driver.find_element(By.XPATH,'//nav[@aria-label="Breadcrumb"]//a[contains(text(),"Introduction")]')
             redirect.click()
             time.sleep(5)
         except Exception as e:
             print(f'error:{e}')
             
+            
+  
+    
+    
     teardown(driver)
     
 get_course_list()
-                       
-            
-              
